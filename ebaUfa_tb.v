cb40983ce38a4b4bb92d7e4240227108ceef8546
Ajaymalolan <115446924+Ajaymalolan@users.noreply.github.com>
2023-05-03 11:40:53 +0530

    Update ebaUfa_tb.v

--- ebaUfa_tb.v
+++ ebaUfa_tb.v
@@ -1,21 +1,30 @@
+//testbech for ebaUfa.v to simulate it.
+
+//begin testbench module.
 module ebaUfa_tb;
 
+// simulation port declaration.
 reg [7:0] a;
 reg [7:0] b;
 reg cin;
 wire [7:0] sum;
 wire cout;
 
+//source file instantiation. 
 ebaUfa e (a,b,cin,sum,cout);
 
+//initiate simulation.
 initial
 begin
 
+//dumping variables and creating output wave form file.
 $dumpfile("b_ebaUfa.vcd");
 $dumpvars(0,ebaUfa_tb);
+
+//monitoring i/o signals.
 $monitor(a,b,cin,sum,cout);
 
-#10 
+//simulating for different values. 
 a=8'b00000001;
 b=8'b00000001;
 cin=1'b0;
@@ -53,6 +62,9 @@ b=8'b11111111;
 cin=1'b1;
 #10
 
+//ending simulation.
 $finish;
 end
+
+//ending module.
 endmodule
