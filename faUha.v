7910d00c73da1c9c571bd00003fbcdc2081147f2
Ajaymalolan <115446924+Ajaymalolan@users.noreply.github.com>
2023-05-03 11:42:52 +0530

    Update faUha.v

--- faUha.v
+++ faUha.v
@@ -1,15 +1,19 @@
-//Full adder using half adder
+//behavioral modeling.
+//full adder using half adder.
 
-//Include half adder from previous program
+//include half adder from previous program
 `include "halfAdder.v"
 
-//Full adder using half adder
+//start module.
 module faUha(a,b,cin,s,c);
 
+//declare i/o ports.
 input a,b,cin;
 output s,c;
 
+//declare intermediate connections.
 wire si,ci,cii;
+
 //first stage
 halfAdder H1 (.a(a),.b(b),.c(ci),.s(si));
 halfAdder H2 (.a(si),.b(cin),.c(cii),.s(s));
@@ -17,4 +21,5 @@ halfAdder H2 (.a(si),.b(cin),.c(cii),.s(s));
 //second stage
 assign c= ci|cii;
 
+//ending module
 endmodule
