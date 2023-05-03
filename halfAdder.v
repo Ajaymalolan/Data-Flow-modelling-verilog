c1c87dd2811c923107de3047e358ecf22f191cab
Ajaymalolan <115446924+Ajaymalolan@users.noreply.github.com>
2023-05-03 11:59:42 +0530

    Update halfAdder.v

--- halfAdder.v
+++ halfAdder.v
@@ -1,8 +1,15 @@
+//behavioral modeling.
+//half adder.
+
+//start module.
 module halfAdder(a,b,c,s);
+    //declare i/o ports.
     input a,b;
     output c,s;
     
+    //assigning sum and carry values.
     assign s= a ^ b;
     assign c= a & b;
 
+//ending module
 endmodule
