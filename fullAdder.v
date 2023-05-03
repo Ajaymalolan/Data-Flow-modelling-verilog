aaecfd94680221bcb15b37dd13e2e4f6123f8717
Ajaymalolan <115446924+Ajaymalolan@users.noreply.github.com>
2023-05-03 11:52:17 +0530

    Update fullAdder.v

--- fullAdder.v
+++ fullAdder.v
@@ -1,9 +1,16 @@
+//behavioral modeling.
+//full adder.
+
+//start module.
 module fullAdder(a,b,cin,cout,s);
 
+//declare i/o ports.
 input a,b,cin;
 output cout,s;
 
+//assigning sum and carry values.
 assign s= (a^b)^cin;
 assign cout=(a&b)|cin;
 
+//ending module
 endmodule
