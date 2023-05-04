b371973bda4547367d5804a055eeeedf636d46ec
Ajaymalolan <115446924+Ajaymalolan@users.noreply.github.com>
2023-05-04 15:03:30 +0530

    Add files via upload

---
+++ halfSubtractor.v
@@ -0,0 +1,14 @@
+//behavioral modelling
+//half subtractor
+module halfSubtractor(a,b,c,d);
+
+//declare i/o ports
+input a,b;
+output c,d;
+
+//assign difference and borrow
+assign d = a ^ b;
+assign c = (~a) & b;
+
+//end module
+endmodule
